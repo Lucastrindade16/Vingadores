afc4e1c6658fc46b906c9973d112b3c1617892d2
Lucastrindade16 <lucastrindade.br64@gmail.com>
2025-01-28 13:25:29 -0300

    build: instalando o flask

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-#from model import vingador
-#from model.interface import Database
-from model.interface import Interface
-# from model.vingador import Vingador
+from flask import Flask, render_template
 
-def main():
+app = Flask(__name__)
 
-    # db = Database()
-    # db.connect()
+@app.route("/")
+def home():
+    return render_template("home.html")
 
-    #query = 'SELECT' * 'FROM heroi'
-    #herois = db.select(query)
-    #for heroi in herois:
-     #   vingador(heroi[1], heroi[2], heroi[3], heroi[4], heroi[5], heroi[6], heroi[7])
+@app.route("/ola")
+def hello_world():
+    return "<p>Olá, mundo!<p>"
 
-    #Interface().menu_principal()
+# from model.interface import Interface
 
-if __name__ == '__main__':
-    main()
+# def main():
+#       interface()
+
+# if __name__ == '__main__':
+    # main()
